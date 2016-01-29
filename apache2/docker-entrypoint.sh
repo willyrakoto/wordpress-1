7dedbbf7ca1d18416a7ee082f9c0359bd8dc6bfe
willyrakoto <willy.rakotoarisoa@gmail.com>
2016-01-29 16:03:09 +0100

    Update docker-entrypoint.sh

--- apache2/docker-entrypoint.sh
+++ apache2/docker-entrypoint.sh
@@ -54,7 +54,7 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 				</IfModule>
 				# END WordPress
 			EOF
-			#chown www-data:www-data .htaccess
+			chown www-data:www-data .htaccess
 		fi
 	fi
 
