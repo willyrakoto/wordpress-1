952de8005acbde66e70d9c69c93031156cc7613e
willyrakoto <willy.rakotoarisoa@gmail.com>
2016-11-30 22:45:25 +0100

    Update docker-entrypoint.sh

--- apache2/docker-entrypoint.sh
+++ apache2/docker-entrypoint.sh
@@ -1,43 +1,51 @@
 #!/bin/bash
-set -e
+set -eu
 
-if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
-
-        # Openshift v3 Define WORDPRESS Databases environment variables
-	if [ -z "$MYSQL_SERVICE_HOST" ]; then
-		echo >&2 'warning: $MYSQL_SERVICE_HOST not found, you should have a mysql service in your project'
-	else
-		: ${WORDPRESS_DB_HOST:=$MYSQL_SERVICE_HOST}
-	fi
-	if [ -z "$MYSQL_SERVICE_PORT" ]; then
-		echo >&2 'warning: $MYSQL_SERVICE_PORT not found, you should have a mysql service in your project'
-	else
-		: ${WORDPRESS_DB_PORT:=$MYSQL_SERVICE_PORT}
-	fi
-	if [ -z "$WORDPRESS_DB_HOST" ]; then
-		echo >&2 'error: missing WORDPRESS_DB_HOST and WORDPRESS_DB_PORT environment variables'
-		echo >&2 '  Did you forget to add a mysql service to your project?'
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
 		exit 1
 	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
 
-	: ${WORDPRESS_DB_USER:=$MYSQL_USER}
-	: ${WORDPRESS_DB_PASSWORD:=$MYSQL_PASSWORD}
-	: ${WORDPRESS_DB_NAME:=$MYSQL_DATABASE}
+if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
+	file_env 'WORDPRESS_DB_HOST' "${MYSQL_SERVICE_HOST:-mysql}"
+	# if we're linked to MySQL and thus have credentials already, let's use them
+	file_env 'WORDPRESS_DB_USER' "${MYSQL_USER:-}"
+	file_env 'WORDPRESS_DB_PASSWORD' "${MYSQL_PASSWORD:-}"
+	file_env 'WORDPRESS_DB_NAME' "${MYSQL_DATABASE:-wordpress}"
+	if [ -z "$WORDPRESS_DB_PASSWORD" ]; then
+		echo >&2 'error: missing required WORDPRESS_DB_PASSWORD environment variable'
+		echo >&2 '  Did you forget to -e WORDPRESS_DB_PASSWORD=... ?'
+		echo >&2
+		echo >&2 '  (Also of interest might be WORDPRESS_DB_USER and WORDPRESS_DB_NAME.)'
+		exit 1
+	fi
 	echo $WORDPRESS_DB_HOST
 	echo $WORDPRESS_DB_PASSWORD
 	echo $WORDPRESS_DB_NAME
 	echo $WORDPRESS_DB_USER
-        echo "Coucou"
-        #End Openshift v3
-
 	if ! [ -e index.php -a -e wp-includes/version.php ]; then
 		echo >&2 "WordPress not found in $(pwd) - copying now..."
 		if [ "$(ls -A)" ]; then
 			echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
-			( set -x; ls -A; sleep 1000 )
+			( set -x; ls -A; sleep 10 )
 		fi
-		#tar cf - --one-file-system -C /usr/src/wordpress . | tar xf -
-		# Openshift v3 issue : copy source instead of using tar
 		cp -R /usr/src/wordpress/* .
 		echo >&2 "Complete! WordPress has been successfully copied to $(pwd)"
 		if [ ! -e .htaccess ]; then
@@ -54,12 +62,16 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 				</IfModule>
 				# END WordPress
 			EOF
-			
 			chmod 1001:root .htaccess
 		fi
 	fi
 
 	# TODO handle WordPress upgrades magically in the same way, but only if wp-includes/version.php's $wp_version is less than /usr/src/wordpress/wp-includes/version.php's $wp_version
+
+	# version 4.4.1 decided to switch to windows line endings, that breaks our seds and awks
+	# https://github.com/docker-library/wordpress/issues/116
+	# https://github.com/WordPress/WordPress/commit/1acedc542fba2482bab88ec70d4bea4b997a92e4
+	sed -ri -e 's/\r\n|\r/\n/g' wp-config*
 
 	if [ ! -e wp-config.php ]; then
 		awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wp-config-sample.php > wp-config.php <<'EOPHP'
@@ -74,13 +86,13 @@ EOPHP
 
 	# see http://stackoverflow.com/a/2705678/433558
 	sed_escape_lhs() {
-		echo "$@" | sed 's/[]\/$*.^|[]/\\&/g'
+		echo "$@" | sed -e 's/[]\/$*.^|[]/\\&/g'
 	}
 	sed_escape_rhs() {
-		echo "$@" | sed 's/[\/&]/\\&/g'
+		echo "$@" | sed -e 's/[\/&]/\\&/g'
 	}
 	php_escape() {
-		php -r 'var_export(('$2') $argv[1]);' "$1"
+		php -r 'var_export(('$2') $argv[1]);' -- "$1"
 	}
 	set_config() {
 		key="$1"
@@ -92,7 +104,7 @@ EOPHP
 			start="^(\s*)$(sed_escape_lhs "$key")\s*="
 			end=";"
 		fi
-		sed -ri "s/($start\s*).*($end)$/\1$(sed_escape_rhs "$(php_escape "$value" "$var_type")")\3/" wp-config.php
+		sed -ri -e "s/($start\s*).*($end)$/\1$(sed_escape_rhs "$(php_escape "$value" "$var_type")")\3/" wp-config.php
 	}
 
 	set_config 'DB_HOST' "$WORDPRESS_DB_HOST"
@@ -112,35 +124,48 @@ EOPHP
 		LOGGED_IN_SALT
 		NONCE_SALT
 	)
+	echo "UNIQUES CREATED"
 	for unique in "${UNIQUES[@]}"; do
-		eval unique_value=\$WORDPRESS_$unique
-		if [ "$unique_value" ]; then
-			set_config "$unique" "$unique_value"
+		uniqVar="WORDPRESS_$unique"
+		file_env "$uniqVar"
+		if [ "${!uniqVar}" ]; then
+			set_config "$unique" "${!uniqVar}"
 		else
 			# if not specified, let's generate a random value
-			current_set="$(sed -rn "s/define\((([\'\"])$unique\2\s*,\s*)(['\"])(.*)\3\);/\4/p" wp-config.php)"
+			current_set="$(sed -rn -e "s/define\((([\'\"])$unique\2\s*,\s*)(['\"])(.*)\3\);/\4/p" wp-config.php)"
 			if [ "$current_set" = 'put your unique phrase here' ]; then
 				set_config "$unique" "$(head -c1M /dev/urandom | sha1sum | cut -d' ' -f1)"
 			fi
 		fi
 	done
-
+	echo "WORDPRESS_TABLE_PREFIX"
+	file_env 'WORDPRESS_TABLE_PREFIX'
 	if [ "$WORDPRESS_TABLE_PREFIX" ]; then
 		set_config '$table_prefix' "$WORDPRESS_TABLE_PREFIX"
 	fi
-
+	echo "WORDPRESS_DEBUG"
+	file_env 'WORDPRESS_DEBUG'
 	if [ "$WORDPRESS_DEBUG" ]; then
 		set_config 'WP_DEBUG' 1 boolean
 	fi
-
+	echo `cat wp-config.php`
 	TERM=dumb php -- "$WORDPRESS_DB_HOST" "$WORDPRESS_DB_USER" "$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" <<'EOPHP'
 <?php
 // database might not exist, so let's try creating it (just to be safe)
 $stderr = fopen('php://stderr', 'w');
-list($host, $port) = explode(':', $argv[1], 2);
+// https://codex.wordpress.org/Editing_wp-config.php#MySQL_Alternate_Port
+//   "hostname:port"
+// https://codex.wordpress.org/Editing_wp-config.php#MySQL_Sockets_or_Pipes
+//   "hostname:unix-socket-path"
+list($host, $socket) = explode(':', $argv[1], 2);
+$port = 0;
+if (is_numeric($socket)) {
+	$port = (int) $socket;
+	$socket = null;
+}
 $maxTries = 10;
 do {
-	$mysql = new mysqli($host, $argv[2], $argv[3], '', (int)$port);
+	$mysql = new mysqli($host, $argv[2], $argv[3], '', $port, $socket);
 	if ($mysql->connect_error) {
 		fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
 		--$maxTries;
