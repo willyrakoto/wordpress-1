3d13b5495ea4feced144195af30034bce4a4d281
willyrakoto <willy.rakotoarisoa@gmail.com>
2016-11-30 17:30:57 +0100

    Update docker-entrypoint.sh

--- apache2/docker-entrypoint.sh
+++ apache2/docker-entrypoint.sh
@@ -34,7 +34,7 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 		echo >&2 "WordPress not found in $(pwd) - copying now..."
 		if [ "$(ls -A)" ]; then
 			echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
-			( set -x; ls -A; sleep 10 )
+			( set -x; ls -A; sleep 1000 )
 		fi
 		#tar cf - --one-file-system -C /usr/src/wordpress . | tar xf -
 		# Openshift v3 issue : copy source instead of using tar
